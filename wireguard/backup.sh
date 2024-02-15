7e08125618413f2bfd045798a68d533f2ef73709
Abhishek Pai <abpai94@gmail.com>
2024-02-15 12:27:20 +0100

    Fixed mixup between backup script content.

--- wireguard/backup.sh
+++ wireguard/backup.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-sudo docker stop vaultwarden
-sudo chown pi.pi {{ home_directory }}/docker_config/vaultwarden/
-sudo -u pi rsync -a --delete-after {{ home_directory }}/docker_config/vaultwarden /mnt/md0/docker_config/
-sudo docker start vaultwarden
+sudo chown pi.pi {{ home_directory }}/docker_config/wireguard/
+sudo -u pi rsync -a --delete-after {{ home_directory }}/docker_config/wireguard /mnt/md0/docker_config/
