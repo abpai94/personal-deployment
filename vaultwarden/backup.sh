e491f14cf5b5158ef9b72ce6a4837b73f25504d4
Abhishek Pai <abpai94@gmail.com>
2024-03-19 11:58:17 +0100

    Deleted backup scripts and cron jobs.

--- vaultwarden/backup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo docker stop vaultwarden
-sudo chown pi.pi {{ home_directory }}/docker_config/vaultwarden/
-sudo -u pi rsync -a --delete-after {{ home_directory }}/docker_config/vaultwarden /mnt/md0/docker_config/
-sudo docker start vaultwarden
